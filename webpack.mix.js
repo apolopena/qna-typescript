5569f2ebe8eee1d8956734a118168a713235df11
apolopena <penavilla@gmail.com>
2021-05-11 05:32:39 +0000

    ➕ ADD: typescript and hot reload

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,26 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .react()
     .sass('resources/sass/app.scss', 'public/css');
+
+/**
+ * BEGIN: Injected from .gp/snippets/laravel/webpack/browser-sync.snippet
+ */
+
+const { execSync } = require('child_process');
+const PROXY_PORT=execSync(`bash $GITPOD_REPO_ROOT/.gp/bash/helpers.sh get_default_server_port`)
+const BS_PORT=3005
+const GP_URL = execSync(`gp url ${BS_PORT}`)
+mix.browserSync({
+  ui: false,
+  proxy: {
+    target: `http://localhost:${PROXY_PORT}`
+  },
+  port: BS_PORT,
+  socket: {
+    domain: GP_URL
+  }
+});
+
+ /**
+ * END: Injected from .gp/snippets/laravel/webpack/browser-sync.snippet
+ */
