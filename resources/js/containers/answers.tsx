633ff2ec794519f9bffe6f70c09dd27270f9b6ed
apolopena <penavilla@gmail.com>
2021-05-19 18:09:26 +0000

    ♻️ REFACTOR: cleanup

--- resources/js/containers/answers.tsx
+++ resources/js/containers/answers.tsx
@@ -11,6 +11,7 @@ export function AnswersContainer() {
   const { questionId } = useParams<{ questionId: string }>()
   const payload = useDataApi<AnswersPayload>(`/api/questions/${questionId}`)
   const [answers, setAnswers] = useState<AnswerData[]>()
+
   /*
   // uncomment for debugging
   console.count('render count')
@@ -19,22 +20,24 @@ export function AnswersContainer() {
     console.log(`the value of payload is ${JSON.stringify(payload, null, 2)}`)
   }, [payload, answers])
   */
+
   const displayAnswers = (payload: Payload<AnswersPayload>) => {
     if (payload.error) return <div className='cardBody py-4 error mt-1'>{payload.error}</div>
     if (!payload.data) return <></>
-    console.log(`the value of answers is ${JSON.stringify(answers, null, 2)}`)
-    console.log(`the value of payload is ${JSON.stringify(payload, null, 2)}`)
+
     const NoAnswers = () => (
       <div className='cardBody py-4'>
         No answers yet! Be the first to answer by using the form below.
       </div>
     )
+
     if (answers) {
       return (answers.length > 0)
         ? <AnswerList answers={answers} />
         : NoAnswers()
     }
-    return (payload.data.answers?.length > 0)
+    
+    return (payload.data.answers.length > 0)
       ? <AnswerList answers={payload.data?.answers} />
       : NoAnswers()
   }
